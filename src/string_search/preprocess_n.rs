54b56cae141f1f9f733e8dcc50886ef5718961df
Arnab De <arkrde@gmail.com>
2022-10-04 15:14:49 +0530

    `preprocess_n` implemented

---
+++ src/string_search/preprocess_n.rs
@@ -0,0 +1,28 @@
+use crate::string_search::preprocess_z;
+
+struct PrepN {
+    prep: preprocess_z::PrepZ,
+}
+
+impl PrepN {
+    fn from_str(text: &str) -> PrepN {
+        let text_rev = text.chars().rev().collect::<String>();
+        PrepN {
+            prep: preprocess_z::PrepZ::from_str(&text_rev),
+        }
+    }
+    fn score(&self, idx: usize) -> Option<usize> {
+        let idx = self.prep.len() - 1 - idx;
+        self.prep.score(idx)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn preprocess_test() {
+        let s = super::PrepN::from_str("cabdabdab");
+        assert_eq!(s.score(2), Some(2));
+        assert_eq!(s.score(5), Some(5));
+    }
+}
