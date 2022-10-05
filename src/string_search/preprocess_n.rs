f9ad4aaa231839fe2c40a39a8412f8daf1623f32
Arnab De <arkrde@gmail.com>
2022-10-05 16:35:56 +0530

    More test cases for preprocess_n

--- src/string_search/preprocess_n.rs
+++ src/string_search/preprocess_n.rs
@@ -33,5 +33,58 @@ mod preprocess_n_tests {
         assert_eq!(s.score(5), Some(5));
         assert_eq!(s.score(6), Some(0));
         assert_eq!(s.score(7), Some(0));
+        assert_eq!(s.score(8), Some(9));
+    }
+
+    #[test]
+    fn preprocess_test_2() {
+        let s = super::PrepN::from_str("abcabcab");
+        assert_eq!(s.score(0), Some(0));
+        assert_eq!(s.score(1), Some(2));
+        assert_eq!(s.score(2), Some(0));
+        assert_eq!(s.score(3), Some(0));
+        assert_eq!(s.score(4), Some(5));
+        assert_eq!(s.score(5), Some(0));
+        assert_eq!(s.score(6), Some(0));
+        assert_eq!(s.score(7), Some(8));
+    }
+
+    #[test]
+    fn preprocess_test_3() {
+        let s = super::PrepN::from_str("abcdefgh");
+        assert_eq!(s.score(0), Some(0));
+        assert_eq!(s.score(1), Some(0));
+        assert_eq!(s.score(2), Some(0));
+        assert_eq!(s.score(3), Some(0));
+        assert_eq!(s.score(4), Some(0));
+        assert_eq!(s.score(5), Some(0));
+        assert_eq!(s.score(6), Some(0));
+        assert_eq!(s.score(7), Some(8));
+    }
+
+    #[test]
+    fn preprocess_test_4() {
+        let s = super::PrepN::from_str("abcdefga");
+        assert_eq!(s.score(0), Some(1));
+        assert_eq!(s.score(1), Some(0));
+        assert_eq!(s.score(2), Some(0));
+        assert_eq!(s.score(3), Some(0));
+        assert_eq!(s.score(4), Some(0));
+        assert_eq!(s.score(5), Some(0));
+        assert_eq!(s.score(6), Some(0));
+        assert_eq!(s.score(7), Some(8));
+    }
+
+    #[test]
+    fn preprocess_test_5() {
+        let s = super::PrepN::from_str("aaaaaaaa");
+        assert_eq!(s.score(0), Some(1));
+        assert_eq!(s.score(1), Some(2));
+        assert_eq!(s.score(2), Some(3));
+        assert_eq!(s.score(3), Some(4));
+        assert_eq!(s.score(4), Some(5));
+        assert_eq!(s.score(5), Some(6));
+        assert_eq!(s.score(6), Some(7));
+        assert_eq!(s.score(7), Some(8));
     }
 }
