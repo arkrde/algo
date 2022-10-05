34143e1940eb8ed7e5df2f8e139c743c2ed4f567
Arnab De <arkrde@gmail.com>
2022-10-05 16:37:21 +0530

    Modules added for Boyer Moore pre-processing helper routines

--- src/string_search.rs
+++ src/string_search.rs
@@ -5,5 +5,16 @@ pub mod naive_search;
 // e.g. BM, KMP, etc.
 mod preprocess_z;
 
+// preprocess-N as preprocessing for Boyer Moore
+mod preprocess_n;
+
+// preprocess-L as preprocessing for Boyer Moore
+mod preprocess_l;
+
+// preprocess-Lprime as preprocessing for Boyer Moore
+mod preprocess_lprime;
 // Simple linear-time exact string search using preprocess-Z
 pub mod linear_search;
+
+// preprocessor for Boyer-Moore search.
+// mod bm_prep;
